d3b42a2dae54e4619d9654ae82d60256b05c04dd
Benjamin Bussell <bsbussell>
2019-08-21 16:40:45 -0500

    MOTOR ( v0.2 ) && SERVO ( b0.0)
    Debugging Motor to work and passing serial as an argument with initalization.
    Started Servo testing, once I know how it will work I'll begin cleaning it.

--- control_scheme/controllers/SpeedController.py
+++ control_scheme/controllers/SpeedController.py
@@ -1,25 +1,40 @@
+'''
+SpeedController.py
+Benjamin S. Bussell
+August 21st 2019
+'''
+
+
 from threading import Thread
 import pyvesc
 import serial
+
+# TODO: DONT LET THIS IMPORT SURVIVE TESTING
 import time
 
+
+# This allows the run function to quietly run in the background.
 def threaded(fn):
     def wrapper(*args, **kwargs):
         thread = Thread(target=fn, args=args, kwargs=kwargs)
         thread.start()
         return thread
+
     return wrapper
 
 
 class MOTOR:
 
-    def __init__(self):
+    def __init__(self, serialConnection):
 
-        # self.FSESC = serial.Serial('PLACEHOLDER',115200,timeout = 0.1)
+        # Uncomment this for the actual program, it is just commented so I can test.
+        self.FSESC = serialConnection
 
+        # Current: the current applied to the motor
         self.current = 0
-        self.brake = 0
+        # Duty Cycle: Controlling work load without accounting for current
         self.dutyCycle = 0
+        self.brake = 0
 
         self.active = True
 
@@ -39,25 +54,27 @@ class MOTOR:
                 # Priortize brake then current then duty cycle
                 if (self.brake != 0):
                     print("Brake: ", self.brake)
-                    #self.FSESC.write(self.brakePacket(self.brake))
+                    self.FSESC.write(self.brakePacket(self.brake))
                 elif (self.current != 0):
                     print("Current: ", self.current)
-                    #self.FSESC.write(self.currentPacket(self.current))
+                    self.FSESC.write(self.currentPacket(self.current))
                 elif (self.dutyCycle != 0):
                     print("Duty Cycle: ", self.dutyCycle)
-                    #self.FSESC.write(self.dutyCyclePacket(self.dutyCycle))
+                    self.FSESC.write(self.dutyCyclePacket(self.dutyCycle))
+                self.FSESC.flush()
+
 
                 self.previousTime = time.time()
 
     def dutyCyclePacket(self, value) -> bytes:
 
-        message = pyvesc.setDutyCycle(value)
+        message = pyvesc.SetDutyCycle(value)
         packet = pyvesc.encode(message)
         return packet
 
     def currentPacket(self, value) -> bytes:
 
-        message = pyvesc.setCurrent(value)
+        message = pyvesc.SetCurrent(value)
         packet = pyvesc.encode(message)
         return packet
 
@@ -67,8 +84,6 @@ class MOTOR:
         packet = pyvesc.encode(message)
         return packet
 
-
-
     def speed_handle(self, value):
         self.current = value
         pass
@@ -77,15 +92,17 @@ class MOTOR:
         self.active = False
         pass
 
-def main():
-    speed = MOTOR()
-    loop = speed.run()
-    speed.speed_handle(5)
-    time.sleep(5)
-    speed.speed_handle(20)
-    time.sleep(1)
-    speed.kill()
 
+def main():
+
+    macbookPort = serial.Serial('/dev/tty.usbmodem3011',115200,timeout = 0.1)
+    speed = MOTOR(macbookPort)
+    loop = speed.run()
+    speed.speed_handle(0)
+    time.sleep(5)
+    speed.speed_handle(2700)
+    time.sleep(15)
+    speed.kill()
 
 
 if __name__ == "__main__":
---
+++ control_scheme/controllers/SteerController.py
@@ -0,0 +1,36 @@
+'''
+SteerController.py
+Benjamin S. Bussell
+August 21st 2019
+'''
+
+
+import pyvesc
+import serial
+
+class SERVO:
+
+    def __init__(self, serialConnection):
+
+        self.FSESC = serialConnection
+
+        self.position = 0
+        pass
+
+
+
+    def buildPositionPacket(self, value):
+
+        message = pyvesc.SetPosition(value)
+        packet = pyvesc.encode(message)
+        return packet
+
+    def set_Steering(self, value):
+
+        self.position = value
+        self.FSESC.write(self.buildPositionPacket(self.position))
+        pass
+
+def main():
+    servo = SERVO()
+    servo.set_Steering(10)
