bb8fafd1cef256b336741bfb237d280454324046
Benjamin Bussell <benjaminsbussell@gmail.com>
2019-08-29 10:10:53 -0500

    Serial port Picker
    Easier testing across platforms

--- test_cases/control_test_mac.py
+++ test_cases/control_test_mac.py
@@ -1,13 +1,14 @@
 
 from control_scheme.SpeedController import Motor
 from control_scheme.SteerController import Servo
-import serial
+
+from test_cases.serial_port_picker import select_Port
 
 import time
 
 def main():
 
-    port = serial.Serial('COM8', 115200, timeout=0.1)
+    port = select_Port()
 
     motor = Motor(port)
     servo = Servo(port)
@@ -15,7 +16,7 @@ def main():
     loop = motor.run()
 
     motor.set_Current(2900)
-    servo.set_Steering(30)
+    servo.set_Steering(100)
 
     time.sleep(5)
 
